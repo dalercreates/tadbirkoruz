d23e8d1f69a94c0da9424289a8ed2245e3245cfa
Shukhrat <63923013+Shukha04@users.noreply.github.com>
2023-04-10 04:11:45 +0500

    Finished mobile nav and so on

--- js/script.js
+++ js/script.js
@@ -8,3 +8,28 @@ dropdownButton.addEventListener('click', () => {
         dropdownButton.classList.add('active')
     }
 })
+
+// mobile nav
+const toggleBtn = document.getElementById('toggle-menu')
+const mobileNav = document.getElementById('mobile__nav')
+
+toggleBtn.addEventListener('click', () => {
+    if (toggleBtn.classList.contains('checked')) {
+        toggleBtn.classList.remove('checked')
+        mobileNav.classList.remove('active')
+    } else {
+        toggleBtn.classList.add('checked')
+        mobileNav.classList.add('active')
+    }
+})
+
+// sticky nav
+const nav = document.getElementById('nav')
+
+document.addEventListener('scroll', () => {
+    if (scrollY > 0) {
+        nav.classList.add('sticky')
+    } else {
+        nav.classList.remove('sticky')
+    }
+})
