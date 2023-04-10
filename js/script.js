b46e0416930e76fa9ba002762db21162f1edfe10
Daler Firuzov <110167520+dalercreates@users.noreply.github.com>
2023-04-10 12:41:11 +0500

    footer section is ready and changed some bugs with info section

--- js/script.js
+++ js/script.js
@@ -3,33 +3,33 @@ const dropdownButton = document.getElementById('dropdown-btn');
 
 dropdownButton.addEventListener('click', () => {
     if (dropdownButton.classList.contains('active')) {
-        dropdownButton.classList.remove('active')
+        dropdownButton.classList.remove('active');
     } else {
-        dropdownButton.classList.add('active')
+        dropdownButton.classList.add('active');
     }
-})
+});
 
 // mobile nav
-const toggleBtn = document.getElementById('toggle-menu')
-const mobileNav = document.getElementById('mobile__nav')
+const toggleBtn = document.getElementById('toggle-menu');
+const mobileNav = document.getElementById('mobile__nav');
 
 toggleBtn.addEventListener('click', () => {
     if (toggleBtn.classList.contains('checked')) {
-        toggleBtn.classList.remove('checked')
-        mobileNav.classList.remove('active')
+        toggleBtn.classList.remove('checked');
+        mobileNav.classList.remove('active');
     } else {
-        toggleBtn.classList.add('checked')
-        mobileNav.classList.add('active')
+        toggleBtn.classList.add('checked');
+        mobileNav.classList.add('active');
     }
-})
+});
 
 // sticky nav
-const nav = document.getElementById('nav')
+const nav = document.getElementById('nav');
 
 document.addEventListener('scroll', () => {
     if (scrollY > 0) {
-        nav.classList.add('sticky')
+        nav.classList.add('sticky');
     } else {
-        nav.classList.remove('sticky')
+        nav.classList.remove('sticky');
     }
-})
+});
